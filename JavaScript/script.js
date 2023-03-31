421439e22a8bc8103b0bba8ce33ea6cc176690a7
Robert Juszczyński <robertjuszczynski@o2.pl>
2023-03-31 17:07:53 +0200

    Hover Effects and 300ms delay remove
    Added 'fastclick' library to remove 300ms delay when clicking buttons on mobile devices. Added hover effects.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,18 @@
 const html = document.querySelector("html");
 const inputs = document.querySelectorAll("input");
-
+/*
+Fast Click Library for eliminating the 300ms delay
+between a physical tap and the firing of a click event on mobile browsers
+*/
+if ("addEventListener" in document) {
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      FastClick.attach(document.body);
+    },
+    false
+  );
+}
 // A script to save themes to the browser's local storage
 function loadTheme() {
   const theme = localStorage.getItem("data-theme");
