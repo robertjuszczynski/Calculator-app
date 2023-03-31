a803e8ee200c8f57d19fdfc887223196c1057328
Robert Juszczyński <robertjuszczynski@o2.pl>
2023-03-31 15:35:20 +0200

    Styles
    Added theme changer with the function of saving in the browser's local memory, improved responsiveness

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const html = document.querySelector("html");
+const inputs = document.querySelectorAll("input");
+
+// A script to save themes to the browser's local storage
+function loadTheme() {
+  const theme = localStorage.getItem("data-theme");
+  if (theme) html.setAttribute("data-theme", theme);
+  inputs.forEach((input) => {
+    input.checked = input.getAttribute("id") === theme;
+  });
+}
+inputs.forEach((input) => {
+  input.addEventListener("click", () => {
+    const theme = input.getAttribute("id");
+    html.setAttribute("data-theme", theme);
+    localStorage.setItem("data-theme", theme);
+  });
+});
+
+// Load theme on page load
+loadTheme();
