103387eada31f07902d710b953f28d24d867cc9c
Robert Juszczyński <robertjuszczynski@o2.pl>
2023-04-02 20:38:07 +0200

    Main logic has been constructed
    There are still a few bugs to be fixed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,12 @@
 const html = document.querySelector("html");
 const inputs = document.querySelectorAll("input");
+const numbers = document.querySelectorAll("[data-number]");
+const operators = document.querySelectorAll("[data-operator]");
+const deleteButton = document.querySelector("[data-delete]");
+const resetButton = document.querySelector("[data-reset]");
+const equalsButton = document.querySelector("[data-equals]");
+const previousOperationText = document.querySelector("[data-previous-operation]");
+const currentOperationText = document.querySelector("[data-current-operation]");
 /*
 Fast Click Library for eliminating the 300ms delay
 between a physical tap and the firing of a click event on mobile browsers
@@ -30,6 +37,83 @@ inputs.forEach((input) => {
     localStorage.setItem("data-theme", theme);
   });
 });
+
+//Calculator class
+class calc {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText
+    this.currentOperationText = currentOperationText
+    this.reset()
+  }
+  delete() {
+    if (!this.currentOperand.toString().slice(0, -1)) {
+      this.currentOperand = '0'
+      return
+    }
+    this.currentOperand = this.currentOperand.slice(0, -1)
+  }
+  reset() {
+    this.previousOperand = ''
+    this.currentOperand = '0'
+  }
+  addNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    if (this.currentOperand === '0') this.currentOperand = '';
+    if ('+-x/'.includes(number) && '+-x/'.includes(this.currentOperand.slice(-1))) {
+      this.currentOperand = `${this.currentOperand.slice(0, -1)}${number}`;
+      return;
+    }
+    this.currentOperand += number;
+  }
+  calculate() {
+    const result = eval(this.currentOperand.replace(/x/g, '*'));
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = parseFloat(result.toFixed(10)).toString();
+  }
+  updateDisplay() {
+    this.currentOperationText.innerText = this.currentOperand
+  }
+}
+
+const calculator = new calc(previousOperationText, currentOperationText)
+
+numbers.forEach(numberButton => {
+  numberButton.addEventListener('click', () => {
+    calculator.addNumber(numberButton.innerText)
+    calculator.updateDisplay();
+  })
+});
+operators.forEach(operatorButton => {
+  operatorButton.addEventListener('click', () => {
+    calculator.addNumber(operatorButton.innerText)
+    calculator.updateDisplay();
+  })
+});
+deleteButton.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateDisplay();
+})
+resetButton.addEventListener('click', () => {
+  calculator.reset();
+  calculator.updateDisplay();
+})
+equalsButton.addEventListener('click', () => {
+  calculator.calculate();
+  calculator.updateDisplay();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Load theme on page load
 loadTheme();
